dc7092c4ed0bdff4dae6bcd89f16cd26638156d3
francescocaccia <cacciapuotifrancesco3@gmail.com>
2023-10-27 14:35:30 +0200

    cors filter

--- src/main/java/Francesco/Iscrizioni/Pugilistica/Fabrianese/auth/CorsConfig.java
+++ src/main/java/Francesco/Iscrizioni/Pugilistica/Fabrianese/auth/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://iscrizioni-palestra-front-end.vercel.app/")
+				registry.addMapping("/**").allowedOrigins("https://iscrizioni-palestra-front-end.vercel.app")
+
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS").allowCredentials(true)
 						.maxAge(3600);
 			}
